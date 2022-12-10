e1f3229b10f33e7d46364a01ceb3e7923659c18a
TerryBusy <33195550+TerryBusy@users.noreply.github.com>
2022-12-10 20:38:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ import time
 import PySimpleGUI as sg
 from pythonosc import udp_client
 
-dictobj = {'Mono': 'track', 'Stereo': 'stereo', 'Speaker': 'speaker'}
-listobj = []
-for i in dictobj:
-    listobj.append(i)
+dict_object = {'Mono': 'track', 'Stereo': 'stereo', 'Speaker': 'speaker'}
+list_object = []
+for i in dict_object:
+    list_object.append(i)
 
-dictco = {'XYZ': ['x', 'y', 'z'], 'AED': ['azim', 'elev', 'dist']}
-listco = []
-for j in dictco:
-    listco.append(j)
+dict_coordinate = {'XYZ': ['x', 'y', 'z'], 'AED': ['azim', 'elev', 'dist']}
+list_coordinate = []
+for j in dict_coordinate:
+    list_coordinate.append(j)
 
 sg.theme('LightGreen')
 layout = [
     [sg.Text('Convert EXCEL file to OSC for Holophonix',  font=("微软雅黑", 10), size=(40, 1), justification='left')],
     [sg.T('Choose Excel file', font=("微软雅黑", 10), size=(40, 1))],
-    [sg.In(key='input', default_text="d:\SPEAKER XYZ.xls"), sg.FileBrowse(key='_BUTTON_KEY_', target='input', file_types=(("excel files", "*.xls"),))],
+    [sg.In(key='input'), sg.FileBrowse(key='_BUTTON_KEY_', target='input', file_types=(("excel file", "*.xls"),))],
     [sg.Text('Choose the type your want to import',  font=("微软雅黑", 10), size=(40, 1), justification='left')],
-    [sg.Drop(listobj, default_value='Speaker', key='xiala1', size=(15, 1), enable_events=True),
-     sg.Drop(listco, default_value='XYZ', key='xiala2', size=(15, 1), enable_events=True)],
-    [sg.T('Holophonix IP address', font=("微软雅黑", 10), size=(18, 1)), sg.In(key='input1', default_text='127.0.0.1')],
-    [sg.T('Holophonix OSC port', font=("微软雅黑", 10), size=(18, 1)), sg.In(key='input2', default_text='4003')],
+    [sg.Drop(list_object, default_value='Speaker', key='-drop1-', size=(15, 1), enable_events=True),
+     sg.Drop(list_coordinate, default_value='XYZ', key='-drop2-', size=(15, 1), enable_events=True)],
+    [sg.T('Holophonix IP address', font=("微软雅黑", 10), size=(18, 1)), sg.In(key='-input1-', default_text='127.0.0.1')],
+    [sg.T('Holophonix OSC port', font=("微软雅黑", 10), size=(18, 1)), sg.In(key='-input2-', default_text='4003')],
     [sg.Button('Send', k='-Button1-')],
     ]
 
@@ -36,38 +36,39 @@ if __name__ == "__main__":
         if event == 'Exit' or event == sg.WIN_CLOSED:
             break
 
-        sendtype = dictobj[values['xiala1']]
-        codin = dictco[values['xiala2']]
-        codina = codin[0]
-        codinb = codin[1]
-        codinc = codin[2]
+        address_type = dict_object[values['-drop1-']]
+        coordinate = dict_coordinate[values['-drop2-']]
+        coordinate_a = coordinate[0]
+        coordinate_b = coordinate[1]
+        coordinate_c = coordinate[2]
 
-        data = xlrd.open_workbook(values['input'])
-        table = data.sheets()[0]
-        numrows = table.nrows
 
         if event == '-Button1-':
             parser = argparse.ArgumentParser()
-            parser.add_argument("--ip", default=(str(values['input1'])),
+            parser.add_argument("--ip", default=(str(values['-input1-'])),
                         help="The ip of the OSC server")
-            parser.add_argument("--port", type=int, default=(int(values['input2'])),
+            parser.add_argument("--port", type=int, default=(int(values['-input2-'])),
                         help="The port the OSC server is listening on")
             args = parser.parse_args()
             client = udp_client.SimpleUDPClient(args.ip, args.port)
 
-            for x in range(1, numrows):
+            data = xlrd.open_workbook(values['input'])
+            table = data.sheets()[0]
+            row_num = table.nrows
+
+            for x in range(1, row_num):
                 name = table.cell_value(x, 0)
-                addressx = '/' + sendtype + '/' + str(x) + '/' + codina
-                addressy = '/' + sendtype + '/' + str(x) + '/' + codinb
-                addressz = '/' + sendtype + '/' + str(x) + '/' + codinc
-                addname = '/' + sendtype + '/' + str(x) + '/' + 'name'
-                xpos = table.cell_value(x, 1)
-                ypos = table.cell_value(x, 2)
-                zpos = table.cell_value(x, 3)
-                client.send_message(addressx, xpos)
-                client.send_message(addressy, ypos)
-                client.send_message(addressz, zpos)
-                client.send_message(addname, name)
+                address_x = '/' + address_type + '/' + str(x) + '/' + coordinate_a
+                address_y = '/' + address_type + '/' + str(x) + '/' + coordinate_b
+                address_z = '/' + address_type + '/' + str(x) + '/' + coordinate_c
+                address_name = '/' + address_type + '/' + str(x) + '/' + 'name'
+                x_pos = table.cell_value(x, 1)
+                y_pos = table.cell_value(x, 2)
+                z_pos = table.cell_value(x, 3)
+                client.send_message(address_x, x_pos)
+                client.send_message(address_y, y_pos)
+                client.send_message(address_z, z_pos)
+                client.send_message(address_name, name)
                 time.sleep(0.15)
 
     window.close()
